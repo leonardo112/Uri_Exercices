8a9c9843424466cc07df14d8e6e295e5b15eba19
leonardo ribeiro <leonardo.ribeirodasvirgens@gmail.com>
2020-03-24 21:34:42 -0300

    Exercises

---
+++ Exercises/Exercise_1001/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise_1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Question - https://www.urionlinejudge.com.br/judge/en/problems/view/1001
+            //Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. Print X as shown below.Print endline after the result otherwise you will get “Presentation Error”.
+
+            //Input
+            //The input file will contain 2 integer numbers.
+
+            //Output
+            //Print the letter X(uppercase) with a blank space before and after the equal signal followed by the value of X, according to the following example.
+
+            //Obs.: don't forget the endline after all.
+
+            int A, B, X;
+                    A = int.Parse(Console.ReadLine());
+                    B = int.Parse(Console.ReadLine());
+                    X = A + B;
+                    Console.WriteLine("X = " + X);
+
+
+
+        }
+    }
+ }
---
+++ Exercises/Exercise_1002/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercise_1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Qusetion - https://www.urionlinejudge.com.br/judge/en/problems/view/1002
+            //The formula to calculate the area of a circumference is defined as A = π.R2.Considering to this problem that π = 3.14159:
+
+            //Calculate the area using the formula given in the problem description.
+
+            //Input
+            //The input contains a value of floating point(double precision), that is the variable R.
+
+            //Present the message "A=" followed by the value of the variable, as in the example bellow, with four places after the decimal point. Use all double precision variables.Like all the problems, don't forget to print the end of line after the result, otherwise you will receive "Presentation Error".
+            //Output
+
+            //code that the URI encourages
+            double A1, n, R1, R2;
+            n = 3.14159;
+            R1 = double.Parse(Console.ReadLine());
+            R2 = R1 * R1;
+            A1 = n * R2;
+            Console.WriteLine("A=" + A1.ToString("F4"));
+
+            //short code
+            double R , π = 3.14159, A ;
+            R = double.Parse(Console.ReadLine());
+            A = π * (R * 2);
+            Console.WriteLine("A= " + A.ToString("F4"));
+
+            //ToString ("F4") is for formatting four decimal places after the comma
+        }
+    }
+}
